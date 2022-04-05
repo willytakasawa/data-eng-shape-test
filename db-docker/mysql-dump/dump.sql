d20bb7fa4f965cbec49a6f0d60db8f36ef8d234f
willytakasawa <w.takasawa.fernandes@gmail.com>
2022-04-05 17:05:04 -0300

    ajustes no processo de insercao mysql

--- db-docker/mysql-dump/dump.sql
+++ db-docker/mysql-dump/dump.sql
@@ -1,3 +1,3 @@
 CREATE TABLE IF NOT EXISTS tb_equipment (equipment_id INTEGER(10) PRIMARY KEY NOT NULL, code VARCHAR(255), group_name VARCHAR(255));
 CREATE TABLE IF NOT EXISTS tb_sensor (sensor_id INTEGER(10) PRIMARY KEY NOT NULL, equipment_id INTEGER(10));
-CREATE TABLE IF NOT EXISTS tb_failure (dt_failure DATETIME NOT NULL, sensor INTEGER(10) NOT NULL, temperature FLOAT(20), vibration FLOAT(20));
+CREATE TABLE IF NOT EXISTS tb_failure (dt_failure DATETIME NOT NULL, sensor INTEGER(10) NOT NULL, temperature FLOAT(20), vibration FLOAT(20), PRIMARY KEY(dt_failure, sensor));
